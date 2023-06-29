5592b9e23cdf89c9fd101f6aebc6557523873875
jrpischel <jrpischel@gmail.com>
2023-06-29 10:55:22 -0400

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+
+		int year = 1977;
+		String month = "May";
+		String weekend = "false";
+
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(weekend);
+		
+	}
+}
