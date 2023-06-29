081e7a23cf7bfbdbfa573a4d972df2f8347780e3
jrpischel <jrpischel@gmail.com>
2023-06-29 11:39:13 -0400

    Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1, num2, num3, average;
+
+		num1 = 5;
+		num2 = 27;
+		num3 = 1977;
+		average = (num1 + num2 + num3)/3;
+
+		System.out.println(average);
+
+	}
+}
