67833b9001a5ff9bc64613ee4ac3a69667748916
jrpischel <jrpischel@gmail.com>
2023-07-05 11:14:55 -0400

    Objective8AllLabs

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int count = 0; count <= 20; count++) {
+			sum = sum + count;
+		}
+		System.out.println(sum);
+	}
+}
